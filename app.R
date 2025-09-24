adbe926b6bc6709443f8e1583e6b814848788eb0
DatViseR <t.stepkowski@nencki.edu.pl>
2025-09-24 14:11:43 +0200

    added debug console log to uploaded files in app.R

--- app.R
+++ app.R
@@ -1,2 +1,11 @@
+cat("WD:", getwd(), "\n")
+cat(
+    "Top-level files:",
+    paste(list.files(".", all.files = TRUE), collapse = ", "),
+    "\n"
+)
+# Example: check a file you read
+# cat("Exists data/foo.csv:", file.exists(here::here("data", "foo.csv")), "\n")
+
 # Rhino / shinyApp entrypoint. Do not edit.
 rhino::app()
